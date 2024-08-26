c5e2174a7680b39b2f30e60a798fd1b14d6928bf
Patrícia Sodré <110927380+PatiPatiSodre@users.noreply.github.com>
2024-08-26 13:50:37 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fluxo de caixa</title>
+</head>
+<body>
+<div id="container">
+        <nav>
+            <ul>
+                <li><a href="cadastro_caixa.html">Cadastro do fluxo de caixa</a></li>
+                <li><a href="listar_caixa.php">Listagem de fluxo de caixa</a></li>
+                <li><a href="consulta_caixa.html">Consulta do saldo do caixa</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
